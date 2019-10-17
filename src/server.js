f34aa5aeffd8c1e1006440d26c0df29032c221d1
Ogo Elumeze <o.elumeze@yahoo.com>
2019-10-17 13:31:50 -0500

    another commit

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const PORT = process.env.PORT || 4300;
+app.listen(PORT);
+
+app.get('/getMovieDetails', (req, res, next) => {
+    console.log("get movie details");
+    let api_url = `https://api.themoviedb.org/3/movie/550?api_key=c43584493dfea10989cdc04f99ffd285`;
+    return axios.get(api_url)
+        .then((response) => {
+            console.log("response", response);
+                return res.status(200).json({
+                    success: true,
+                    error: false,
+                    data: response
+                })
+        }).catch((_error) => {
+            return res.status(400).json({
+                success: false,
+                error: true,
+                message: _error.message
+            })
+        })
+})
+
+console.log(`serve is listening on ${PORT}`);
+
+export default app;
