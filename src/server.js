3c7c63d90c4f9cc09a60849828cc9b7cc2980632
Ogo Elumeze <o.elumeze@yahoo.com>
2019-10-17 13:55:07 -0500

    add the heroku profile start script

--- src/server.js
+++ src/server.js
@@ -10,13 +10,14 @@ app.get('/getMovieDetails', (req, res, next) => {
     let api_url = `https://api.themoviedb.org/3/movie/550?api_key=c43584493dfea10989cdc04f99ffd285`;
     return axios.get(api_url)
         .then((response) => {
-            console.log("response", response);
+            // console.log("response", response);
                 return res.status(200).json({
                     success: true,
                     error: false,
                     data: response
                 })
         }).catch((_error) => {
+            console.log("its an error")
             return res.status(400).json({
                 success: false,
                 error: true,
